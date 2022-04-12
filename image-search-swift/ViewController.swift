3c29fbb3f80999010c81a97879b817b5c635533d
nald-dev <rynvva@gmail.com>
2022-04-13 06:16:46 +0700

    Initial Commit

---
+++ image-search-swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  image-search-swift
+//
+//  Created by Digital on 13/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ image-search-swiftTests/image_search_swiftTests.swift
@@ -0,0 +1,33 @@
+//
+//  image_search_swiftTests.swift
+//  image-search-swiftTests
+//
+//  Created by Digital on 13/04/22.
+//
+
+import XCTest
+@testable import image_search_swift
+
+class image_search_swiftTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
